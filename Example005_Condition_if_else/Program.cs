b7c5e11e8f3d9c1889d4a293a18226e9481c190a
Sh-OV <shchegolga@yandex.ru>
2022-06-25 14:09:06 +0500

    Example005_Condition_if_else end

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите своё имя: ");
+string user_name = Console.ReadLine();
+if (user_name.ToLower() == "маша")
+    {
+        Console.WriteLine("Ура!!! Это же МАША!!!");
+    }
+else
+    {
+        Console.WriteLine("Привет, " + user_name);
+    }
