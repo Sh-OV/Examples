f7c657e9d6bf1eeac3061f6fe705416074d14ed6
Sh-OV <shchegolga@yandex.ru>
2022-07-05 15:35:50 +0500

    Example012_Methods добавили сортировку массива с макс до мин

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -80,3 +80,23 @@ void SelectionSort (int[] array)
 PrintArray(arr);
 SelectionSort(arr);
 PrintArray(arr);
+
+void SelectionSortMax (int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition = i;  
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+SelectionSortMax(arr);
+PrintArray(arr);
